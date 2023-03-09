4ba8b854c6fc355e63a4ab76d360606ac59235e9
Wasimul Vari Sami <76864957+wvsr@users.noreply.github.com>
2023-03-09 23:33:05 +0600

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ function Footer() {
     <footer className='py-8 bg-zinc-100'>
       <div className='text-center text-lg font-bold'>
         Made with ♥ by{' '}
-        <a href='https://wvsr.github.com' className='text-[#b82d24]'>
+        <a href='https://wvsr.github.io' className='text-[#b82d24]'>
           Sami.
         </a>
       </div>
